11bf8e2917172914b22a03e9d560f27ea4d2ed04
fai516 <yfsiuv@gmail.com>
2022-04-25 12:44:38 +0800

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,8 @@
-import React from "react";
-import { CacheProvider, EmotionCache } from "@emotion/react";
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-import createEmotionCache from "../utility/createEmotionCache";
-import lightTheme from "../styles/theme/lightTheme";
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-const clientSideEmotionCache = createEmotionCache();
-
-const MyApp = ({ Component, pageProps, emotionCache }: MyAppProps) => {
-  emotionCache = clientSideEmotionCache;
-  return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />'
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
-  );
-};
-
-export default MyApp;
+export default MyApp
